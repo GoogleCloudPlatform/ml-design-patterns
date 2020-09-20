b634d0d58ce3dfc08678799ee2a6b585fe2b2394
V Lakshmanan <lak@vlakshman.com>
2020-09-20 20:51:49 +0000

    preprocessing working; embedding still has problems

--- 06_reproducibility/storm_reports/preprocess.py
+++ 06_reproducibility/storm_reports/preprocess.py
@@ -1,8 +1,18 @@
+
+import tensorflow_hub as hub
+swivel = hub.load("https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1")
+
 def preprocessing_fn(inputs):
-    import re
+    import tensorflow as tf
     
     outputs = inputs.copy()
     comments = inputs['comments']
-    outputs['office'] = re.compile(r"\([A-Z]+\)$").search(comments).group(0)[1:-1]
-    outputs['comments'] = comments[:-5].lower().strip()
+    outputs['office'] = tf.strings.substr(comments, -4, 3)
+    comments = tf.strings.regex_replace(comments, r"\([A-Z]+\)$", "")
+    outputs['comments'] = tf.strings.lower(comments)
+    #if len(outputs['comments'].shape) == 0:
+    #    swivel_input = [outputs['comments']]
+    #else:
+    #    swivel_input = outputs['comments']
+    #outputs['embed'] = swivel(swivel_input)
     return outputs
