2206267d5e6ad5a8e22820027c692c95145b3684
V Lakshmanan <lak@vlakshman.com>
2020-10-16 00:08:40 +0000

    Find good set of hyperparameters for small batch

--- 02_data_representation/weather_search/overfit_batch.sh
+++ 02_data_representation/weather_search/overfit_batch.sh
@@ -15,8 +15,11 @@ gcloud ai-platform jobs submit training $JOB_NAME \
         --module-name $MODULE_NAME \
         --job-dir gs://${BUCKET}/wxsearch/trained \
         --region $REGION \
-        --config hyperparam.yaml \
-        -- \
+        --config train.yaml -- --num_layers 4 --pool_size 4 --num_filters 4 --num_dense 50 \
         --input gs://${BUCKET}/wxsearch/data/2019/tfrecord-00000-* \
         --project ${PROJECT} \
-        --batch_size 4 --num_steps 1000 --num_checkpoints 2
+        --batch_size 4 --num_steps 1000 --num_checkpoints 4
+        
+        
+        
+#        --config hyperparam.yaml -- \
--- 02_data_representation/weather_search/wxsearch/train_autoencoder.py
+++ 02_data_representation/weather_search/wxsearch/train_autoencoder.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 import logging
 import hypertune
 import argparse
@@ -22,33 +23,64 @@ def read_dataset(pattern):
     ds = tf.data.TFRecordDataset(filenames, compression_type=None, buffer_size=None, num_parallel_reads=None)
     return ds.prefetch(tf.data.experimental.AUTOTUNE).map(parse_tfrecord)
 
-def create_model(nlayers=4, poolsize=4, embed_size=256):
+
+def create_model(nlayers=4, poolsize=4, numfilters=5, num_dense=0):
     input_img = tf.keras.Input(shape=(1059, 1799, 1), name='refc_input')
 
     x = tf.keras.layers.Cropping2D(cropping=((17, 18),(4, 3)), name='cropped')(input_img)
     last_pool_layer = None
     for layerno in range(nlayers):
-        x = tf.keras.layers.Conv2D(2**(nlayers-layerno + 3), 5, activation='relu', padding='same', name='encoder_conv_{}'.format(layerno))(x)
+        x = tf.keras.layers.Conv2D(2**(layerno + numfilters), poolsize, activation='relu', padding='same', name='encoder_conv_{}'.format(layerno))(x)
         last_pool_layer = tf.keras.layers.MaxPooling2D(poolsize, padding='same', name='encoder_pool_{}'.format(layerno))
         x = last_pool_layer(x)
     output_shape = last_pool_layer.output_shape[1:]
     
-    # flatten input
-    x = tf.keras.layers.Flatten(name='encoder_flatten')(x)
-    x = tf.keras.layers.Dense(embed_size, name='refc_embedding')(x)
-    
-    # decode, but starting with the shape of the last preflattened layer
-    x = tf.keras.layers.Dense(output_shape[0] * output_shape[1] * output_shape[2], name='decoder_input')(x)
+    if num_dense == 0:
+        # flatten to create the embedding
+        x = tf.keras.layers.Flatten(name='refc_embedding')(x)
+        embed_size = output_shape[0] * output_shape[1] * output_shape[2]
+        if embed_size > 1024:
+            print("Embedding size={} is too large".format(embed_size))
+            return None, embed_size
+    else:
+        # flatten, send through dense layer to create the embedding
+        x = tf.keras.layers.Flatten(name='encoder_flatten')(x)
+        x = tf.keras.layers.Dense(num_dense, name='refc_embedding')(x)
+        x = tf.keras.layers.Dense(output_shape[0] * output_shape[1] * output_shape[2], name='decoder_dense')(x)
+        embed_size = num_dense
+        
     x = tf.keras.layers.Reshape(output_shape, name='decoder_reshape')(x)
     for layerno in range(nlayers):
-        x = tf.keras.layers.Conv2D(2**(layerno + 4), 5, activation='relu', padding='same', name='decoder_conv_{}'.format(layerno))(x)
+        x = tf.keras.layers.Conv2D(2**(nlayers-layerno-1 + numfilters), poolsize, activation='relu', padding='same', name='decoder_conv_{}'.format(layerno))(x)
         x = tf.keras.layers.UpSampling2D(poolsize, name='decoder_upsamp_{}'.format(layerno))(x)
-    x = tf.keras.layers.Conv2D(1, 3, activation='relu', padding='same', name='before_padding')(x)
-    decoded = tf.keras.layers.ZeroPadding2D(padding=((17,18),(4,3)), name='refc_reconstructed')(x)
+    before_padding_layer = tf.keras.layers.Conv2D(1, 3, activation='relu', padding='same', name='before_padding')
+    x = before_padding_layer(x)
+    htdiff = 1059 - before_padding_layer.output_shape[1]
+    wddiff = 1799 - before_padding_layer.output_shape[2]
+    if htdiff < 0 or wddiff < 0:
+        print("Invalid architecture: htdiff={} wddiff={}".format(htdiff, wddiff))
+        return None, 9999
+    decoded = tf.keras.layers.ZeroPadding2D(padding=((htdiff//2,htdiff - htdiff//2),
+                                                     (wddiff//2,wddiff - wddiff//2)), name='refc_reconstructed')(x)
 
     autoencoder = tf.keras.Model(input_img, decoded, name='autoencoder')
     autoencoder.compile(optimizer='adam', loss=tf.keras.losses.LogCosh()) #loss='mse')
-    return autoencoder
+    if autoencoder.count_params() > 1000*1000: # 1 million      
+        print("Autoencoder too large: {} params".format(autoencoder.count_params()))
+        return None, autoencoder.count_params()
+    
+    return autoencoder, embed_size
+
+class HptCallback(tf.keras.callbacks.Callback):
+    def __init__(self):
+        self.hpt = hypertune.HyperTune()
+    
+    def on_epoch_end(self, epoch, logs):
+        self.hpt.report_hyperparameter_tuning_metric(
+            hyperparameter_metric_tag='final_loss',
+            metric_value=logs['loss'],   #history.history['loss'][-1],
+            global_step=epoch
+        )
 
 def run_job(opts):
     def input_and_label(rec):
@@ -59,21 +91,16 @@ def run_job(opts):
     
     strategy = tf.distribute.MirroredStrategy()
     with strategy.scope():
-        autoencoder = create_model(opts['num_layers'], opts['pool_size'])
-        print(autoencoder)
-        history = autoencoder.fit(ds, steps_per_epoch=opts['num_steps']//opts['num_checkpoints'],
-                              epochs=opts['num_checkpoints'], shuffle=True, callbacks=[checkpoint])
+        autoencoder, error = create_model(opts['num_layers'], opts['pool_size'], opts['num_filters'], opts['num_dense'])
+       
+        if autoencoder:    
+            print(autoencoder.summary())
+            history = autoencoder.fit(ds, steps_per_epoch=opts['num_steps']//opts['num_checkpoints'],
+                              epochs=opts['num_checkpoints'], shuffle=True, callbacks=[checkpoint, HptCallback()])
     
-        autoencoder.save(os.path.join(opts['job_dir'], 'savedmodel'))
-        
-        # report final metric to hyperparameter tuner
-        hpt = hypertune.HyperTune()
-        hpt.report_hyperparameter_tuning_metric(
-            hyperparameter_metric_tag='final_loss',
-            metric_value=history.history['loss'][-1],
-            global_step=1
-        )
-    
+            autoencoder.save(os.path.join(opts['job_dir'], 'savedmodel'))
+        else:
+            HptCallback().on_epoch_end(1, {'loss': error})
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -97,7 +124,9 @@ if __name__ == '__main__':
     parser.add_argument(
       '--pool_size', default=4, help='size of upscaling/downscaling kernel', type=int)    
     parser.add_argument(
-      '--embed_size', default=256, help='size of embedding', type=int)    
+      '--num_filters', default=4, help='efficiency of representation of a tile', type=int)    
+    parser.add_argument(
+      '--num_dense', default=50, help='size of embedding if you want a dense layer. Specify 0 to use conv layers only', type=int)    
     
      
     # parse command-line args and add a few more
